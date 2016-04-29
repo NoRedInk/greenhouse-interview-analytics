a4cd7ecc9bfeed6f3a22adfd277f95441616bb3d
Marica Odagaki <ento.entotto@gmail.com>
2016-04-28 22:23:14 -0700

    Better tag cleaning

--- tagging.py
+++ tagging.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import itertools
 import string
 import re
 
@@ -6,6 +7,7 @@ from stop_words import get_stop_words
 
 
 TAG_DELIM_RE = re.compile(r'([.,;()]? +|\.$)')
+COMPOUND_TAG_DELIM_RE = re.compile(r'([-_])')
 PUNCT_ERASER = dict.fromkeys(map(ord, string.punctuation + ' '))
 
 
@@ -14,10 +16,21 @@ def split_tags(tags):
         return []
     stop_words = get_stop_words('en')
     for tag in TAG_DELIM_RE.split(tags):
-        print(tag)
         if tag in stop_words:
             continue
-        stripped = tag.strip()
-        cleaned = tag.translate(PUNCT_ERASER)
-        if stripped and cleaned:
-            yield stripped.lower()
+        cleaned = clean_tag(tag)
+        if cleaned:
+            yield cleaned.lower()
+
+
+def clean_tag(tag):
+    '''
+    >>> clean_tag('(solar-eclipse)')
+    'solar-eclipse'
+    '''
+    if not tag.translate(PUNCT_ERASER):
+        return
+    parts = COMPOUND_TAG_DELIM_RE.split(tag.strip())
+    words = [part.translate(PUNCT_ERASER) for part in parts[::2]]
+    delims = parts[1::2]
+    return ''.join(itertools.chain(*itertools.zip_longest(words, delims, fillvalue='')))
