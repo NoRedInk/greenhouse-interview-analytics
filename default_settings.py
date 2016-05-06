a4a705e7254ea08d67129cbe9b9151c0e840258a
Marica Odagaki <ento.entotto@gmail.com>
2016-05-06 07:15:11 -0700

    Add comments to settings

--- default_settings.py
+++ default_settings.py
@@ -3,8 +3,11 @@ import os
 HERE = os.path.dirname(__file__)
 DATASET_DATABASE_URI = 'sqlite:///' + os.path.join(HERE, 'data', 'interviews.db')
 
-TAGS_QUESTION_RE = None
+# If set, pull tags only from those scorecard questions that match this pattern.
+TAGS_QUESTION_RE = None # r'^What was the question'
+# If set, pull tags only from those lines that start with this string.
 TAGS_PREFIX = 'tags:'
 
+# If set, adds basic auth at the Flask level.
 BASIC_AUTH_USERNAME = None
 BASIC_AUTH_PASSWORD = None
